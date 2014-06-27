0bf72888de5346bbd832bacaa3f975607f2b4558
Nils Mehnert <mehnert@vecam.de>
2014-06-27 13:01:06 +0200

    View-Formular funktioniert jetzt. Nur das Validieren wirft immer ein false.

--- src/Vest/SymfonyBundle/Controller/ViewController.php
+++ src/Vest/SymfonyBundle/Controller/ViewController.php
@@ -2,6 +2,7 @@
 
 use Vest\SymfonyBundle\Entity\Resumfrage;     // Zeigt dem Controller, wo er die Entität findet (verwendet in Zeilte 17)
 use Vest\SymfonyBundle\Entity\resfragenliste; 
+use Vest\SymfonyBundle\Entity\Resantwort; 
 use Vest\SymfonyBundle\Entity\Resfrage;     // Zeigt dem Controller, wo er die Entität findet (verwendet in Zeilte 17)
 use Symfony\Component\HttpFoundation\Request;   // Zeigt dem Controller wo er die Funktionen für Requests findet.
               
@@ -18,11 +19,11 @@ class ViewController extends BaseController
 				$frageliste = new resfragenliste();
 				$frageliste->setU_Id($u_id);
 				$frageliste->setU_Name($umfrage->getName());
-				$frageliste->setFeld1($umfrage->getFeld1());
-				$frageliste->setFeld2($umfrage->getFeld2());
-				$frageliste->setFeld3($umfrage->getFeld3());
-				$frageliste->setFeld4($umfrage->getFeld4());
-				$frageliste->setFeld5($umfrage->getFeld5());
+				$frageliste->setFeld1($feld1);
+				$frageliste->setFeld2($feld2);
+				$frageliste->setFeld3($feld3);
+				$frageliste->setFeld4($feld4);
+				$frageliste->setFeld5($feld5);
 				$repository_fragen = $this->getDoctrine()->getRepository('VestSymfonyBundle:Resfrage');// Baut Doctrine auf 
 				$fragen = $repository_fragen->findBy(array('uId' => $u_id));  //hohlt das passende Objekt aus der Doctrine. Wichtig, damit man danach updaten kann.    
      				
@@ -49,7 +50,7 @@ class ViewController extends BaseController
 					->add('feld4', 'hidden', array('data' => $feld4))
 					->add('feld5', 'hidden', array('data' => $feld5));
 					
-					$form = $form->add('antwort', 'collection', array('type'=> 'email','options'=>array()));
+					$form = $form->add('antwort', 'collection', array('type'=> 'text','options'=>array('label' => false)));
 					$form = $form->add('f_id', 'collection', array('type'=> 'hidden'));
 					
 					// Weiterlesen und passend einbauen: http://symfony.com/doc/current/reference/forms/types/collection.html
@@ -62,6 +63,77 @@ class ViewController extends BaseController
 				return $this->render('VestSymfonyBundle:Default:view_form.html.twig', array(
 					'form' => $form->createView(), 'type' => 'new', 'u_name' =>$u_name, 'fragen' => $frage_tmp
 				));
+	}
+	
+	public function ErhaltenAction(Request $request, $u_id){
+			$umfrage = new Resumfrage(); // legt eine neue Person an 
+				$repository_umfrage = $this->getDoctrine()->getRepository('VestSymfonyBundle:Resumfrage');// Baut Doctrine auf 
+				$umfrage = $repository_umfrage->findOneBy(array('id' => $u_id));  //hohlt das passende Objekt aus der Doctrine. Wichtig, damit man danach updaten kann.    
+        
+				$u_name = $umfrage->getName();
+				
+				$frageliste = new resfragenliste();
+				$frageliste->setU_Id($u_id);
+				$frageliste->setU_Name($umfrage->getName());
+				$repository_fragen = $this->getDoctrine()->getRepository('VestSymfonyBundle:Resfrage');// Baut Doctrine auf 
+				$fragen = $repository_fragen->findBy(array('uId' => $u_id));  //hohlt das passende Objekt aus der Doctrine. Wichtig, damit man danach updaten kann.    
+     				
+				$frage_tmp = array();
+				$f_id_tmp = array();
+				$antwort_tmp = array();
+				foreach($fragen as $frage){
+					$frage_tmp[] = $frage->getFrage();
+					$f_id_tmp[]= $frage->getId();
+				}
+				$frageliste->setFrage($frage_tmp);
+				$frageliste->setFId($f_id_tmp);
+				
+				
+				
+				$form = $this->createFormBuilder($frageliste) // Formular wird auf Basis der Person erstellt   
+					->setAction($this->generateUrl('Responsa_view_erhalten',array('u_id' => $u_id)))     //nimmt die URL aus der routing.yml 
+					->add('u_id', 'hidden', array('data' => $u_id))
+					->add('feld1', 'hidden', array('data' => ''))
+					->add('feld2', 'hidden', array('data' => ''))
+					->add('feld3', 'hidden', array('data' => ''))
+					->add('feld4', 'hidden', array('data' => ''))
+					->add('feld5', 'hidden', array('data' => ''));
+					
+					$form = $form->add('antwort', 'collection', array('type'=> 'text','options'=>array('label' => false)));
+					$form = $form->add('f_id', 'collection', array('type'=> 'hidden'));
+					
+					
+						
+				$form  = $form->add('Speichern', 'submit');
+				$form  = $form->getForm();
+				
+				$form->handleRequest($request); 
+				if ($form->isValid()) {
+					$count = 0;
+					foreach($frageliste.antwort as $aw){
+						$ant = new Resantwort();
+						$ant->setAntwort($aw);
+						$ant->setFId($f_id_tmp[$count]);
+						
+						$ant->setAuswertungsbereich('noch nicht eingebaut');
+						$ant->setFType('noch nicht eingebaut');
+						
+							$em = $this->getDoctrine()->getManager();
+							$em->persist($ant);
+							$em->flush();
+					
+						$count ++;
+					}
+					return $this->render('VestSymfonyBundle:Default:view_eingetragen.html.twig', array(
+						'eingetragen' => true
+					));
+				}	
+					$errors = $form->getErrorsAsString();
+					return $this->render('VestSymfonyBundle:Default:view_eingetragen.html.twig', array(
+						'eingetragen' => false, 'errors' => $errors
+					));
+				
+	
 	}
 	
 }
