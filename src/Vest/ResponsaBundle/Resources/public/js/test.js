0710c0a1cb71509c812eaf0815accb5c2de07a22
timmermann-vecam <timmermann@vecam.de>
2014-06-27 16:54:24 +0200

    Bootstrap CSS GEHT JS nicht

---
+++ src/Vest/ResponsaBundle/Resources/public/js/test.js
@@ -0,0 +1,4 @@
+/**
+ * Created by admin on 27.06.14.
+ */
+console.log("CUSTOM JAVASCRIPT FILE LOADED");
--- src/Vest/SymfonyBundle/Security/User/User.php
+++ src/Vest/SymfonyBundle/Security/User/User.php
@@ -5,19 +5,19 @@ use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Session\Storage\PhpBridgeSessionStorage;
-use Vest\SymfonyBundle\Entity\AddrPersonen; 
+use Vest\SymfonyBundle\Entity\AddrPersonen;
 
 class User
 {
-		
+
     private $person_id;
     private $kontakt_id;
     private $email;
 		private $roles;
-		
-	
 
-		
+
+
+
 		function __construct($k_id = 0, $p_id = 0, $mail = null) {
 			$this->person_id = $p_id;
 			$this->kontakt_id = $k_id;
@@ -32,27 +32,30 @@ class User
 			echo 'person_id: '.$this->person_id.'<br />';
 			echo 'kontakt_id: '.$this->kontakt_id.'<br />';
 		}
-		
+
 		function getDATA(){
-		
+
 			$result = array();
 			$result[] = $this->person_id;
 			$result[] = $this->kontakt_id;
 			$result[] = $this->email;
-			if($this->roles != null){
-				foreach($this->$roles as $role){
-					$result[] = $role;
-				}
-			}else{
-				$result[] = 'Keine Rollen vorhanden oder Fehler beim auslesen der Rollen';
-			}
+
+
+
+//			if($this->roles != null){
+//				foreach($this->$roles as $role){
+//					$result[] = $role;
+//				}
+//			}else{
+//				$result[] = 'Keine Rollen vorhanden oder Fehler beim auslesen der Rollen';
+//			}
 			return $result;
-		
+
 		}
-		
+
 		function setRoles($roles_tmp){
-				
+
 			$this->roles = $roles_tmp;
 		}
-		
+
 }
